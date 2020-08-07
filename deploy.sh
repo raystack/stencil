fbdd0199d8bfca272961403689f3f8d03ced0e76
mauliksoneji <maulik.soneji@go-jek.com>
2020-08-07 14:14:37 +0530

    nexus-setup: add release step to publish to central maven

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_49353df1995f_key -iv $encrypted_49353df1995f_iv -in key.gpg.enc -out key.gpg -d
 echo "Deploying staging artifact"
-./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=key.gpg
-./gradlew closeAndReleaseRepository -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=key.gpg
+./gradlew publishToSonatype closeAndReleaseRepository -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=key.gpg
