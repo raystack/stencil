65e1c53cc9fa58932b0aee4a68592dc9cf631a71
mauliksoneji <maulik.soneji@go-jek.com>
2021-03-09 17:00:07 +0530

    update: command for releasing artifacts to sonatype

--- clients/java/deploy.sh
+++ clients/java/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Deploying staging artifact"
-./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=private.key
+./gradlew clean  publishToSonatype closeAndReleaseStagingRepository -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=private.key --console=verbose
