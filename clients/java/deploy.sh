846b7e9fe56a7ef9fdcbeafaacfae88c484c94ae
mauliksoneji <maulik.soneji@go-jek.com>
2021-03-10 15:04:49 +0530

    ci: create release for new tag

--- clients/java/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "Deploying staging artifact"
-./gradlew clean  publishToSonatype closeAndReleaseStagingRepository -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=private.key --console=verbose
