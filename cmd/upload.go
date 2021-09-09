3c0b8e4fabbd3e6f1a19e561e4a4f47411f42c9c
Harikrishna <harikrishnakanchi@gmail.com>
2021-09-09 18:24:41 +0530

    feat: add all available flags to upload command

--- cmd/upload.go
+++ cmd/upload.go
@@ -2,19 +2,22 @@ package cmd
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"io/ioutil"
-	"log"
 
 	stencilv1 "github.com/odpf/stencil/server/odpf/stencil/v1"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 // UploadCmd creates a new cobra command for upload
 func UploadCmd() *cobra.Command {
 
-	var host, namespace, name, version, filePath string
-	var latest bool
+	var host, filePath string
+	var req stencilv1.UploadDescriptorRequest
+	var skipRules []string
 
 	cmd := &cobra.Command{
 		Use:   "upload",
@@ -23,40 +26,50 @@ func UploadCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fileData, err := ioutil.ReadFile(filePath)
 			if err != nil {
-				log.Fatalln("Unable to read provided file", err)
+				return err
+			}
+			req.Data = fileData
+			req.Checks = &stencilv1.Checks{
+				Except: toRules(skipRules),
 			}
 			conn, err := grpc.Dial(host, grpc.WithInsecure())
 			if err != nil {
-				log.Fatalln(err)
+				return err
 			}
 			defer conn.Close()
 			client := stencilv1.NewStencilServiceClient(conn)
-			ur := &stencilv1.UploadDescriptorRequest{
-				Namespace: namespace,
-				Name:      name,
-				Version:   version,
-				Latest:    latest,
-				Data:      fileData,
-			}
-			res, err := client.UploadDescriptor(context.Background(), ur)
+			_, err = client.UploadDescriptor(context.Background(), &req)
 			if err != nil {
-				log.Fatalln(err)
+				errStatus := status.Convert(err)
+				return errors.New(errStatus.Message())
 			}
-			log.Println(res)
+			fmt.Println("success")
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(&host, "host", "a", "", "stencil host address eg: localhost:8000")
+	cmd.Flags().StringVar(&host, "host", "", "stencil host address eg: localhost:8000")
 	cmd.MarkFlagRequired("host")
-	cmd.Flags().StringVarP(&namespace, "namespace", "g", "", "provide namespace/group or entity name")
+	cmd.Flags().StringVar(&req.Namespace, "namespace", "", "provide namespace/group or entity name")
 	cmd.MarkFlagRequired("namespace")
-	cmd.Flags().StringVarP(&name, "name", "n", "", "provide proto repo name")
+	cmd.Flags().StringVar(&req.Name, "name", "", "provide proto repo name")
 	cmd.MarkFlagRequired("name")
-	cmd.Flags().StringVarP(&version, "version", "v", "", "provide semantic version compatible value")
+	cmd.Flags().StringVar(&req.Version, "version", "", "provide semantic version compatible value")
 	cmd.MarkFlagRequired("version")
-	cmd.Flags().BoolVarP(&latest, "latest", "l", false, "mark as latest version")
-	cmd.Flags().StringVarP(&filePath, "file", "f", "", "provide path to fully contained file descriptor set file")
+	cmd.Flags().BoolVar(&req.Latest, "latest", false, "mark as latest version")
+	cmd.Flags().StringVar(&filePath, "file", "", "provide path to fully contained file descriptor set file")
 	cmd.MarkFlagRequired("file")
+	cmd.Flags().BoolVar(&req.Dryrun, "dryrun", false, "enable dryrun flag")
+	cmd.Flags().StringArrayVar(&skipRules, "skiprules", []string{}, "list of rules to skip. Invalid rules ignored Eg: FILE_NO_BREAKING_CHANGE")
 	return cmd
+}
+
+func toRules(stringRules []string) []stencilv1.Rule {
+	var rules []stencilv1.Rule
+	for _, rule := range stringRules {
+		if val, ok := stencilv1.Rule_value[rule]; ok {
+			rules = append(rules, stencilv1.Rule(val))
+		}
+	}
+	return rules
 }
