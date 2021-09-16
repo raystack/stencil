3f8f4f6bde3f46e687f74bcabbacfa13c93ae6b7
Muhamad Tohir <maztohir@gmail.com>
2021-09-16 17:17:10 +0700

    feat: setup client and message factory
    Co-Authored-By: Rahmat Hidayat <rahmatramahidayat@gmail.com>

--- clients/python/src/odpf/stencil.py
+++ clients/python/src/odpf/stencil.py
@@ -1,10 +1,32 @@
+from clients.python.src.odpf import store
+from schedule import Scheduler
+from google.protobuf.message import Message
+
+from store import Store
 class MultiUrlClient:
-    def __init__(self, urls:list) -> None:
-        pass
-    
-    
+    def __init__(self, urls:list, interval=3600, auto_refresh=False) -> None:
+        self._store = Store()
+        self._urls = urls
+        self._interval = interval
+        self._auto_refresh = auto_refresh
+        self._init_data()
+        self._schduler = Scheduler()
+        if self._auto_refresh:
+            self._schduler.every(self._interval).seconds.do(self.refresh)
+        #TODO: check whether scheduler executed immediatelly or not
+        self.refresh()        
 
+    def refresh(self):
+        for url in self._urls:
+            self._store.load(url)
 
+    def get_descriptor(self, name:str) -> Message:
+        return self._store.get(name)
+    
+    def parse(self, name:str, data:str):
+        msg = self.get_descriptor(name)
+        return msg.ParseFromString(data)
+    
 class Client(MultiUrlClient):
     def __init__(self, url: str) -> None:
         super().__init__([url])
--- clients/python/src/odpf/store.py
+++ clients/python/src/odpf/store.py
@@ -1,13 +1,14 @@
+from google.protobuf.message import Message
 import requests
 from google.protobuf.descriptor_pb2 import FileDescriptorSet
-from google.protobuf.descriptor import Descriptor
+from google.protobuf.message import Message
 from google.protobuf.message_factory import GetMessages
 
 class Store:
     def __init__(self):
         self.data = {}
     
-    def get(self, name) -> Descriptor:
+    def get(self, name) -> Message:
         return self.data.get(name)
     
     def load(self, url):
