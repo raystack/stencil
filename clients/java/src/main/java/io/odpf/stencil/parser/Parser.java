618949d1179cde49fb2c99e18299c15217da4677
Harikrishna <harikrishnakanchi@gmail.com>
2022-01-03 22:53:33 +0530

    feat(java-client): add parse method to StencilClient
    BREAKING CHANGE: removed parser package

--- clients/java/src/main/java/io/odpf/stencil/parser/Parser.java
+++
@@ -1,9 +0,0 @@
-package io.odpf.stencil.parser;
-
-import com.google.protobuf.AbstractMessage;
-import com.google.protobuf.InvalidProtocolBufferException;
-
-public interface Parser {
-    AbstractMessage parse(byte[] data) throws InvalidProtocolBufferException;
-}
-
