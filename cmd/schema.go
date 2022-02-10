39c8e06aefaa8ad13799abff50dcfae6edd8a719
Ishan Arya <ishanarya0@gmail.com>
2022-02-10 11:38:27 +0530

    refactor(cli): combine if statements

--- cmd/schema.go
+++ cmd/schema.go
@@ -289,21 +289,19 @@ func getSchemaCmd() *cobra.Command {
 
 			fmt.Printf("Schema successfully written to %s\n", output)
 
-			if metadata {
-				if resMetadata != nil {
-					report := [][]string{}
+			if metadata && resMetadata != nil {
+				report := [][]string{}
 
-					fmt.Printf("\nMETADATA\n")
-					report = append(report, []string{"FORMAT", "COMPATIBILITY", "AUTHORITY"})
+				fmt.Printf("\nMETADATA\n")
+				report = append(report, []string{"FORMAT", "COMPATIBILITY", "AUTHORITY"})
 
-					report = append(report, []string{
-						stencilv1beta1.Schema_Format_name[int32(resMetadata.GetFormat())],
-						stencilv1beta1.Schema_Compatibility_name[int32(resMetadata.GetCompatibility())],
-						resMetadata.GetAuthority(),
-					})
+				report = append(report, []string{
+					stencilv1beta1.Schema_Format_name[int32(resMetadata.GetFormat())],
+					stencilv1beta1.Schema_Compatibility_name[int32(resMetadata.GetCompatibility())],
+					resMetadata.GetAuthority(),
+				})
 
-					printer.Table(os.Stdout, report)
-				}
+				printer.Table(os.Stdout, report)
 			}
 			return nil
 		},
