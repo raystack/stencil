af8c1dacdc22d323fb154935ab317692a1e8ad44
Vibhu Garg <g.vibhu05@gmail.com>
2022-03-10 09:51:26 +0530

    docs(clojure-client): add clojure client documentation

--- clients/clojure/project.clj
+++ clients/clojure/project.clj
@@ -5,6 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [io.odpf/stencil "0.2.0"]]
+  :plugins [[lein-cljfmt "0.7.0"]]
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src"]
   :repl-options {:init-ns stencil.core})
--- clients/clojure/src/stencil/core.clj
+++ clients/clojure/src/stencil/core.clj
@@ -7,43 +7,43 @@
    (io.odpf.stencil.cache SchemaRefreshStrategy)
    (io.odpf.stencil.exception StencilRuntimeException)
    (io.odpf.stencil.config StencilConfig)
-   (org.apache.http.message BasicHeader)))
+   (org.apache.http.message BasicHeader) (java.util ArrayList)))
 
 (defn create-client
-  "convenience function to create stencil java client instance. Users can use stencil java API directly to create stencil client instance.
+  "Returns a new Stencil Clojure client instance by passing client-config.
 
-   ### Client config structure :
-   | Key                    | Type      | Description                                                                                |
-   | -----------------------|-----------|--------------------------------------------------------------------------------------------|
-   | `url`                  | _String_  | Stencil url                                                                                |
-   | `refresh-cache`        | _Boolean_ | Whether the cache should be refreshed or not                                               |
-   | `refresh-ttl`          | _Integer_ | Cache TTL in minutes                                                                       |
-   | `request-timeout`      | _Integer_ | Request timeout in milliseconds                                                            |
-   | `request-backoff-time` | _Integer_ | Request back off time in minutes                                                           |
-   | `retry-count`          | _Integer_ | Number of retries to be made to fetch descriptor sets                                      |
-   | `headers`              | _Map_     | Map with key as header key and value as header value, will be passed to stencil server     |
-   | `refresh-strategy`     | _keyword_ | Possible values :version-based-refresh, :long-polling-refresh. Default:long-polling-refresh|
+       ### Client config structure :
+       | Key                    | Type      | Description                                                                                 |
+       | -----------------------|-----------|---------------------------------------------------------------------------------------------|
+       | `url`                  | _String_  | Stencil url to fetch latest schema                                                          |
+       | `refresh-cache`        | _Boolean_ | Whether the cache should be refreshed or not                                                |
+       | `refresh-ttl`          | _Integer_ | Cache TTL in minutes                                                                        |
+       | `request-timeout`      | _Integer_ | Request timeout in milliseconds                                                             |
+       | `request-backoff-time` | _Integer_ | Request back off time in minutes                                                            |
+       | `retry-count`          | _Integer_ | Number of retries to be made to fetch schema                                                |
+       | `headers`              | _Map_     | Map with key as header key and value as header value, which will be passed to stencil server|
+       | `refresh-strategy`     | _keyword_ | Possible values :version-based-refresh, :long-polling-refresh. Default :long-polling-refresh|
 
-   Example:
-   ```clojure
-   (let [sample-client-config {:url       \"https://example-url\"
-                              :refresh-cache        true
-                              :refresh-ttl          100
-                              :request-timeout      10000
-                              :request-backoff-time 100
-                              :retry-count          3
-                              :headers              {\"Authorization\" \"Bearer <token>\"}
-                              :refresh-strategy     :version-based-refresh
-                              }]
-         (create-client sample-client-config))
-   ```"
+       Example:
+       ```clojure
+       (let [sample-client-config {:url       \"https://example-url\"
+                                  :refresh-cache        true
+                                  :refresh-ttl          100
+                                  :request-timeout      10000
+                                  :request-backoff-time 100
+                                  :retry-count          3
+                                  :headers              {\"Authorization\" \"Bearer <token>\"}
+                                  :refresh-strategy     :version-based-refresh
+                                  }]
+             (create-client sample-client-config))
+       ```"
   [client-config]
   (try (let [stencil-config (-> (StencilConfig/builder)
                                 (.fetchTimeoutMs (int (:request-timeout client-config)))
                                 (.fetchRetries (int (:retry-count client-config)))
                                 (.cacheAutoRefresh (:refresh-cache client-config))
                                 (.cacheTtlMs (long (:refresh-ttl client-config)))
-                                (.fetchHeaders (let [array-list (new java.util.ArrayList)]
+                                (.fetchHeaders (let [array-list (new ArrayList)]
                                                  (doseq [[k v] (:headers client-config)]
                                                    (.add array-list (BasicHeader. k v)))))
                                 (.refreshStrategy (if (= :version-based-refresh (:refresh-strategy client-config))
@@ -56,19 +56,19 @@
                                                                                         :info  e})))))
 
 (defn get-descriptor
-  "returns protobuf descriptor given a stencil client and classname"
-  [^StencilClient client class-name]
-  (.get client class-name))
+  "Returns protobuf descriptor object for the given protobuf class name."
+  [^StencilClient client proto-class-name]
+  (.get client proto-class-name))
 
 (defn deserialize
-  "returns clojure map for given protobuf encoded byte data and class name"
-  [^StencilClient client class-name data]
-  (-> (get-descriptor client class-name)
+  "Returns Clojure map for the given protobuf encoded byte array and protobuf class name."
+  [^StencilClient client proto-class-name data]
+  (-> (get-descriptor client proto-class-name)
       (bytes->map data)))
 
 (defn serialize
-  "returns byte array for given clojure map and class name"
-  [^StencilClient client class-name map]
-  (-> (get-descriptor client class-name)
+  "Returns protobuf encoded byte array for the given Clojure and protobuf class name."
+  [^StencilClient client proto-class-name map]
+  (-> (get-descriptor client proto-class-name)
       (map->bytes map)))
 
