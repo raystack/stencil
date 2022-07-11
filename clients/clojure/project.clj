cef33432526dfe42028cb592be1b8ba307a96b78
Hari krishna <harikrishnakanchi@gmail.com>
2022-07-11 16:37:22 +0530

    chore: Release 0.2.2 (#136)

--- clients/clojure/project.clj
+++ clients/clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject io.odpf/stencil-clj "0.2.1"
+(defproject io.odpf/stencil-clj "0.2.2"
   :description "Stencil client for clojure"
   :url "https://github.com/odpf/stencil"
   :license {:name "Apache 2.0"
