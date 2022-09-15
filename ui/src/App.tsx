9e6734031d2573fa7ff38ff2c4c48fdd93df4b32
Ravi Suhag <suhag.ravi@gmail.com>
2022-09-14 21:01:33 -0500

    chore: add ui build process

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,3 @@
-
-// import { Breadcrumb, Layout, Menu } from "antd";
-import React, { useEffect, useState } from "react";
-import { getNamespaces } from "./api";
-
-// const { Header, Content, Footer } = Layout;
-
 function App() {
   return (
   <div>hello</div>
