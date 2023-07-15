926dcc7079724abc785efb7e472070f2fffbe852
Ravi Suhag <suhag.ravi@gmail.com>
2023-07-14 19:11:04 -0500

    chore: fallback to odpf version

--- clients/clojure/project.clj
+++ clients/clojure/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.odpf/stencil "0.2.1"]]
+                 [org.raystack/stencil "0.4.0"]]
   :plugins [[lein-cljfmt "0.7.0"]]
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src"]
