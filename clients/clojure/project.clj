941e04932631e127efa2398eb4eb444ff3cec606
Ravi Suhag <suhag.ravi@gmail.com>
2023-07-18 00:46:46 -0500

    chore: upgrade protos (#164)
    * chore: fix typos
    
    * build: run make ui before release
    
    * chore: update protobuf
    
    * build: version bump

--- clients/clojure/project.clj
+++ clients/clojure/project.clj
@@ -1,10 +1,10 @@
-(defproject org.raystack/stencil-clj "0.4.0"
+(defproject org.raystack/stencil-clj "0.4.1"
   :description "Stencil client for clojure"
   :url "https://github.com/raystack/stencil"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.raystack/stencil "0.2.1"]]
+                 [org.raystack/stencil "0.4.0"]]
   :plugins [[lein-cljfmt "0.7.0"]]
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src"]
--- cmd/help.go
+++ cmd/help.go
@@ -7,7 +7,7 @@ import (
 var envHelp = map[string]string{
 	"short": "List of supported environment variables",
 	"long": heredoc.Doc(`
-			ODPF_CONFIG_DIR: the directory where stencil will store configuration files. Default:
+			RAYSTACK_CONFIG_DIR: the directory where stencil will store configuration files. Default:
 			"$XDG_CONFIG_HOME/raystack" or "$HOME/.config/raystack".
 
 			NO_COLOR: set to any value to avoid printing ANSI escape sequences for color output.
