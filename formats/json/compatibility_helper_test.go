8515119a63bf22771dea60a8ffd3baa9d535c260
Punit Kulal <punitkulal1996@gmail.com>
2023-09-25 16:36:29 +0530

    added test for reference check

--- formats/json/compatibility_helper_test.go
+++ formats/json/compatibility_helper_test.go
@@ -39,3 +39,37 @@ func Test_CheckEnum_NoPanic_WhenBothSchemaAreNonEnum(t *testing.T){
 	checkEnum(prev, curr, diffs)
 	assert.Empty(t, diffs.diffs)
 }
+
+func Test_CheckRef_ForSuccess_WhenRefIsSame(t *testing.T){
+	prev := initialiseSchema(t, "./testdata/refChange/prev.json").Properties["roleRef"]
+	curr := initialiseSchema(t, "./testdata/refChange/prev.json").Properties["roleRef"]
+	diffs := &compatibilityErr{notAllowed: backwardCompatibility}
+	checkRef(prev, curr, diffs)
+	assert.Empty(t, diffs.diffs)
+}
+
+func Test_CheckRef_ForSuccess_WhenRefIsAbsentInSchemas(t *testing.T){
+	prev := initialiseSchema(t, "./testdata/refChange/prev.json").Properties["roles"]
+	curr := initialiseSchema(t, "./testdata/refChange/prev.json").Properties["roles"]
+	diffs := &compatibilityErr{notAllowed: backwardCompatibility}
+	checkRef(prev, curr, diffs)
+	assert.Empty(t, diffs.diffs)
+}
+
+func Test_CheckRef_ForFailure_WhenRefIsRemoved(t *testing.T){
+	prev := initialiseSchema(t, "./testdata/refChange/prev.json").Properties["roleRef"]
+	curr := initialiseSchema(t, "./testdata/refChange/removed.json").Properties["roleRef"]
+	diffs := &compatibilityErr{notAllowed: backwardCompatibility}
+	checkRef(prev, curr, diffs)
+	assert.Equal(t, 1, len(diffs.diffs))
+	assert.Equal(t, refChanged, diffs.diffs[0].kind)
+}
+
+func Test_CheckRef_ForFailure_WhenRefIsModified(t *testing.T){
+	prev := initialiseSchema(t, "./testdata/refChange/prev.json").Properties["roleRef"]
+	curr := initialiseSchema(t, "./testdata/refChange/modified.json").Properties["roleRef"]
+	diffs := &compatibilityErr{notAllowed: backwardCompatibility}
+	checkRef(prev, curr, diffs)
+	assert.Equal(t, 1, len(diffs.diffs))
+	assert.Equal(t, refChanged, diffs.diffs[0].kind)
+}
